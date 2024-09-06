d3129d36c5090e9b94d412e7410db92b9374a5e1
Daniel Cohen <danieljcoh@gmail.com>
2024-09-06 17:27:31 -0400

    functionality for retrieving save()d database data onto a index.html

--- django_project_bookstore/book_outlet_app/views.py
+++ django_project_bookstore/book_outlet_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all()  # caching
+    return render(request, "book_outlet_app/index.html", {
+        "books": books
+
+    })
