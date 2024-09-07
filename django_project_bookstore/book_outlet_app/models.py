3bb03689cb85033a35d9e3194ed8cb89ff84bc15
Daniel Cohen <danieljcoh@gmail.com>
2024-09-06 22:16:03 -0400

    added logic to templates and a get_absolute_url to models

--- django_project_bookstore/book_outlet_app/models.py
+++ django_project_bookstore/book_outlet_app/models.py
@@ -1,6 +1,7 @@
 from django.core import validators
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,6 +13,9 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(10)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[self.id])
 
     def __str__(self):
         return f"{self.title}({self.rating})"
--- django_project_bookstore/book_outlet_app/views.py
+++ django_project_bookstore/book_outlet_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Book
 
 # Create your views here.
@@ -8,5 +9,19 @@ def index(request):
     books = Book.objects.all()  # caching
     return render(request, "book_outlet_app/index.html", {
         "books": books
+    })
 
+
+def book_detail(request, id):
+    # book = get_object_or_404(Book, pk=id)
+    try:
+        book = Book.objects.get(pk=id)
+    except:
+        raise Http404()
+
+    return render(request, "book_outlet_app/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
     })
